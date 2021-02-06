3010f594963da6dddd96b55072932b9f3f54b9a0
darkiee <mdipakpatidar@gmail.com>
2021-02-06 13:48:56 +0530

    ..

--- account/urls.py
+++ account/urls.py
@@ -4,12 +4,9 @@ from . import views
 
 
 urlpatterns = [
-<<<<<<< HEAD
-    path('signin/',views.SignInView.as_view(),name='signin'),
+
+   
+    path('',views.SignInView.as_view(),name='signin'),
     path('logout/',views.LogoutView.as_view(),name='logout'),
     path('role/',views.RolesPermissions.as_view(),name='role'),
-=======
-    path('',views.SignInView.as_view(),name='signin'),
-    path('logout/',views.LogoutView.as_view(),name='logout')
->>>>>>> 7abf9e4a360230e45adacca79b2f4098581ab58a
 ]
